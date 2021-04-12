4e0751a423b737bba42fd485a0e4b940eb56859d
kamidem <77845156+kamidem@users.noreply.github.com>
2021-04-12 15:38:51 +0300

    fixed blog js code to fit only 3 posts

--- js/components/blog/Blog.js
+++ js/components/blog/Blog.js
@@ -1,7 +1,5 @@
 class Blog {
   constructor(selector, data) {
-    console.log(selector);
-    console.log(data);
     this.selector = selector;
     this.data = data;
 
@@ -70,45 +68,21 @@ class Blog {
   render() {
     let HTML = '';
 
-    // for (let i = 0; i < this.data.list.length; i++) {
-    //   let blogCount = 0;
+    for (let i = 0; i < 3; i++) {
+      const blogPost = this.data.list[i];
 
-    //   if (blogCount < 4) {
-    //     HTML += `<div class="col-12 col-lg-4 col-md-6 blog-block">
-    //             <img class='blog-image' src="./img/blog/${item.img}.jpeg" alt="blog image">
-    //               <div>
-    //                 <div class="row">
-    //                   <div class="col-12">
-    //                     <div class="tiny-intro">
-    //                       <p><i class="fa fa-calendar-check-o"></i> ${item.date}</p>
-    //                       <p><i class="fa fa-user-o"></i> Admin</p>
-    //                       <p> <span>${item.comment_no}</span> Comments</p>
-    //                     </div>
-    //                     <h3><a href="./img/blog/blog-2.jpeg">${item.title}</a></h3>
-    //                     <p class="main-blog-text">${item.text}</p>
-    //                     <button class="btn">READ MORE</button>
-    //                   </div>
-    //                 </div>
-    //               </div>
-    //         </div>`;
-    //   } else {
-    //     break;
-    //   }
-    // }
-
-    for (const item of this.data.list) {
       HTML += `<div class="col-12 col-lg-4 col-md-6 blog-block">
-                <img class='blog-image' src="./img/blog/${item.img}.jpeg" alt="blog image">
+                <img class='blog-image' src="./img/blog/${blogPost.img}.jpeg" alt="blog image">
                   <div>
                     <div class="row">
                       <div class="col-12">
                         <div class="tiny-intro">
-                          <p><i class="fa fa-calendar-check-o"></i> ${item.date}</p>
+                          <p><i class="fa fa-calendar-check-o"></i> ${blogPost.date}</p>
                           <p><i class="fa fa-user-o"></i> Admin</p>
-                          <p> <span>${item.comment_no}</span> Comments</p>
+                          <p> <span>${blogPost.comment_no}</span> Comments</p>
                         </div>
-                        <h3><a href="./img/blog/blog-2.jpeg">${item.title}</a></h3>
-                        <p class="main-blog-text">${item.text}</p>
+                        <h3><a href="./img/blog/blog-2.jpeg">${blogPost.title}</a></h3>
+                        <p class="main-blog-text">${blogPost.text}</p>
                         <button class="btn">READ MORE</button>
                       </div>
                     </div>
