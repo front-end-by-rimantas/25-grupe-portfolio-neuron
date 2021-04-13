883e4db236ea720a4a9c42332a850804cf98bc6e
kamidem <77845156+kamidem@users.noreply.github.com>
2021-04-13 17:18:40 +0300

    added header logo

---
+++ js/components/header/Logo.js
@@ -0,0 +1,40 @@
+class Logo {
+  constructor(DOM, data) {
+    console.log(data);
+    this.DOM = DOM;
+    this.data = data;
+
+    this.init();
+  }
+
+  init() {
+    if (!this.isValidDOM) {
+      return false;
+    }
+    if (!this.isValidData) {
+      return false;
+    }
+
+    this.render();
+  }
+
+  isValidDOM() {
+    return true;
+  }
+
+  isValidData() {
+    return true;
+  }
+
+  render() {
+
+    let HTML = '';
+
+    const { imgPath, logo, alt } = this.data;
+    HTML = `<img class="logo" src="${imgPath + logo}" alt="${alt}">`;
+
+    this.DOM.innerHTML = HTML;
+  }
+}
+
+export { Logo }
